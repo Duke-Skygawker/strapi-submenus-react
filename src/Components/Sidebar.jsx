0b151b60779e7da258f68dd65cca59c8fffa4691
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-15 19:32:37 +0100

    dynamically populate sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,14 +1,35 @@
 import { useGlobalContext } from "../Context";
 
 const Sidebar = () => {
-  const { closeCross, showSidebar, closeSidebar } = useGlobalContext();
+  const { closeCross, showSidebar, closeSidebar, sublinks } =
+    useGlobalContext();
   return (
     <div className={showSidebar ? "sidebar show-sidebar" : "sidebar"}>
       <div className="sidebar-container">
         <button className="close-btn" onClick={closeSidebar}>
           {closeCross}
         </button>
-        <h4>asasdasd</h4>
+        {sublinks.map((link) => {
+          const { pageId, page, links } = link;
+          return (
+            <article key={pageId}>
+              <h4>{page}</h4>
+              <ul className="sidebar-sublinks">
+                {links.map((item) => {
+                  const { id, label, icon, url } = item;
+                  return (
+                    <li key={id}>
+                      <a href={url}>
+                        <i>{icon}</i>
+                        {label}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </article>
+          );
+        })}
       </div>
     </div>
   );
--- src/Components/Submenu.jsx
+++ src/Components/Submenu.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useGlobalContext } from "../Context";
 
 const Submenu = () => {
