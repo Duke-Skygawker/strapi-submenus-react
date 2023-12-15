49e1b0afa294dfa83457c6a0f078c972882a5200
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-15 19:44:35 +0100

    fixed navbar responsivness

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,16 @@ import NavLinks from "./NavLinks";
 const Navbar = () => {
   const { openSidebar, faBars } = useGlobalContext();
   return (
-    <nav className="nav-center">
-      <h3 className="logo">strapi</h3>
-      <div>
-        <NavLinks />
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <div>
+          <NavLinks />
+        </div>
+        <button className="toggle-btn" onClick={openSidebar}>
+          {faBars}
+        </button>
       </div>
-      <button className="toggle-btn" onClick={openSidebar}>
-        {faBars}
-      </button>
     </nav>
   );
 };
